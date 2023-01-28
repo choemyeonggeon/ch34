62b0de1d5708c8142f03203e4ac6b0108dfea5e7
audrj <audrj@DESKTOP-OUT4L4R>
2023-01-28 21:59:43 +0900

    연습문제

---
+++ src/my/qwe/ch9.java
@@ -0,0 +1,42 @@
+package my.qwe;
+
+import java.util.Scanner;
+
+public class ch9 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("학년을 입력하세요.");
+		int a = sc.nextInt();
+		System.out.print("반을 입력하세요.");
+		int b = sc.nextInt();
+		System.out.print("번호을 입력하세요.");
+		int c = sc.nextInt();
+		
+		System.out.print(a);
+		System.out.printf("%02d", b);
+		System.out.printf("%03d", c);
+	/*	학번을 입력 받아 다음 형식으로 출력하시오.
+		학번은 학년, 반, 번호로 입력된다.
+		이번에는 학년은 한자리, 반은 두자리, 번호는 세자리로 출력한다.
+		예)
+		2 1 20  ===> 201020  으로 출력
+		2 2 7 ==> 202007 으로 출력
+		2 3 100 ==>  203100 으로 출력
+		2 10 111 ==> 210111 로 출력
+		입력
+		학년, 반, 번호가 차례대로 입력된다.(정수)
+		입력범위)
+		학년: 3이하, 반: 20반이하, 번호, 999번 이하
+		출력
+		위의 예를 보고 학번이 총6자리가 되게 출력하시오. 빈 부분은 0으로 채운다.	 
+		입력 예시 
+		2
+		7
+		15
+		출력 예시
+		207015*/
+		
+	}
+
+}
