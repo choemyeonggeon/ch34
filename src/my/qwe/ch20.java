b571434657eb236692dae11f7ca466592095877a
audrj <audrj@DESKTOP-OUT4L4R>
2023-03-04 19:22:21 +0900

    연습문제

---
+++ src/my/qwe/ch20.java
@@ -0,0 +1,26 @@
+package my.qwe;
+
+import java.util.Scanner;
+
+public class ch20 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count = 0; //약수 카운트
+		for(int i=1; i<=n; i++) {
+			if(n % i == 0) { //나누어 떨어짐.
+				count++;
+			}
+		}
+		if(count == 2) {
+			System.out.println("소수임");
+			System.out.println("약수갯수:"+count);
+		}else {
+			System.out.println("소수가 아님");
+			System.out.println("약수갯수:"+count);
+		}
+	}
+
+}
